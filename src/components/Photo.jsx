5d7da00389b7945f2c580ff00a8b76dc4bc8f189
ewelina.slepko <ewelina.slepko1@gmail.com>
2019-05-06 20:10:08 +0200

    Added flexbox

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 
-const Photo = props => (<div style={style.container}>
-    <h1 > {props.photo.title} </h1>
-    <img src={props.photo.url} alt={props.photo.title} style={style.imgWrapper} />
-    <p> {props.photo.explanation} style={style.explonationWrapper}
-    </p>
+const Photo = props => (<div >
+    <h1 style={style.imgWrapper}> {props.photo.title}  </h1>
+    <div style={style.container}>
+        <img src={props.photo.url} alt={props.photo.title} style={style.imgWrapper} />
+        <p style={style.explonationWrapper} > {props.photo.explanation} </p>
+    </div>
 </div>);
 
 
 const style = {
     container: {
+        display: 'flex',
+    },
+    titleWrapper: {
 
     },
     imgWrapper: {
-
+        width: '60vh',
+        height: '60vh',
+        margin: 20
     },
     explanationWrapper: {
-
+        margin: 20
     }
 }
 
