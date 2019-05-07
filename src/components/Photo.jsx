e85862c08264bb55abbbb33470490fd64f25b871
ewelina.slepko <ewelina.slepko1@gmail.com>
2019-05-07 22:33:03 +0200

    Corrected flexbox

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,28 +1,52 @@
 import React from "react";
 
-const Photo = props => (<div >
-    <h1 style={style.imgWrapper}> {props.photo.title}  </h1>
+const Photo = props => (<div>
+
     <div style={style.container}>
-        <img src={props.photo.url} alt={props.photo.title} style={style.imgWrapper} />
-        <p style={style.explonationWrapper} > {props.photo.explanation} </p>
+
+        <div style={style.imgFlex}> <img src={props.photo.url} alt={props.photo.title} /> </div>
+
+        <div style={style.explanationFlex}>
+            <h1 style={style.titleWrapper}>{props.photo.title}</h1>
+            <p>{props.photo.explanation}</p>
+        </div>
+
     </div>
 </div>);
 
 
 const style = {
+
     container: {
+        padding: 3,
+        margin: '0 auto',
         display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        width: '100%',
     },
+
     titleWrapper: {
+        textAlign: 'center',
+        color: '#a5b0b9',
+    },
+
+    imgFlex: {
+        margin: 0,
+        height: '40%',
+        width: '40%',
+        maxWidth: '50%',
+        padding: '20px 0px'
 
     },
-    imgWrapper: {
-        width: '60vh',
-        height: '60vh',
-        margin: 20
-    },
-    explanationWrapper: {
-        margin: 20
+
+    explanationFlex: {
+        maxWidth: '50%',
+        lineHeight: 1.4,
+        fontSize: 20,
+        margin: '0px 72px',
+        padding: '0px 6px',
+        color: '#5c6168'
     }
 }
 
