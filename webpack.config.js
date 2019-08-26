407fb672c956aa92bafa48c4df68ad4b28994905
ewelina.slepko <slepko.ewelina@gmail.com>
2019-08-26 22:00:05 +0200

    Added Progressive Web Apps to application

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const PUBLIC_PATH = 'https://nasa-get-the-photo.firebaseapp.com/';
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: PUBLIC_PATH
     },
     module: {
         rules: [{
@@ -63,6 +67,32 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: './src/index.html'
         }),
-        new WorkboxPlugin.GenerateSW()
+        new WorkboxPlugin.GenerateSW(),
+        new SWPrecacheWebpackPlugin(
+            {
+                cacheId: 'my-domain-cache-id',
+                dontCacheBustUrlsMatching: /\.\w{8}\./,
+                filename: 'service-worker.js',
+                minify: true,
+                navigateFallback: PUBLIC_PATH + 'index.html',
+                staticFileGlobsIgnorePatterns: [/\.map$/, /manifest\.json$/]
+            }
+        ),
+        new WebpackPwaManifest({
+            name: 'NASA - Picture of the day!',
+            short_name: 'NASA Picture',
+            description: '',
+            background_color: '#252526',
+            theme_color: '#262626',
+            'theme-color': '#262626',
+            start_url: '/',
+            icons: [
+                {
+                    src: path.resolve('src/images/planets512.png'),
+                    sizes: [57, 96, 128, 192, 256, 384, 512],
+                    destination: path.join('assets', 'icons')
+                }
+            ]
+        })
     ]
 }
